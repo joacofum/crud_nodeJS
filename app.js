ba1dfcb4839063714169f0a75ebe403105f50e02
Joaquin <joacofum@gmail.com>
2022-09-26 17:56:11 -0300

    ultimo commit

--- app.js
+++ app.js
@@ -2,22 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
-
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
-});
-
-//Modelo de pelicula
-const PeliculaModel = require('./models/pelicula')
-
 // Conexión a Base de datos
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://peliculas:peliculas@clusterpeliculas.jvfynvf.mongodb.net/peliculasDB?retryWrites=true&w=majority";
 
-mongoose.connect(uri, {
+//Cosas del enviromment
+require('dotenv').config()
+
+mongoose.connect(process.env.URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 })
@@ -25,15 +16,22 @@ mongoose.connect(uri, {
 	.catch(e => console.log(e))
 
 
+//Modelo de pelicula
+const PeliculaModel = require('./models/pelicula')
 
-//Find
-const find = async() => {
+app.get("/", async (req, res) => {
 	const peliculas = await PeliculaModel.find()
-	console.log(peliculas)
-}
+	res.send(peliculas);
+});
 
-//Add
-const add = async() => {
+app.get("/findById/:id", async (req, res) => {
+	let peliculaId = req.params.id;
+	console.log(peliculaId)
+	const peliculas = await PeliculaModel.findById({_id: peliculaId})
+	res.send(peliculas);
+});
+
+app.post("/add", async (req, res) => {
 	const pelicula = new PeliculaModel({
 		name: "Shrek 2",
 		year: 2003,
@@ -41,27 +39,30 @@ const add = async() => {
 		cast: ["Mike Myers", "Eddie Murphy", "Chris Miller", "Conrad Vernon"],
 		genres: ["Comedy", "Fantasy", "Animated"]
 	})
-	const resultado = await pelicula.save()
-	console.log(resultado);
-}
+	await pelicula.save()
+});
 
-//Update by _id
-const update = async(id) => {
-	const pelicula = await PeliculaModel.updateOne({_id:id},
-	{
-		$set:{
-			name: 'Shrek',
-			year: 2001
-		}
-	})
-}
+app.put("/updateById/:id", async (req, res) => {
+	let peliculaId = req.params.id;
+	await PeliculaModel.updateOne({_id: peliculaId},
+		{
+			$set:{
+				name: 'Shrek',
+				year: 2001
+			}
+		})
+});
 
-//Delete by _id
-const removeById = async(id) => {
-	const pelicula = await PeliculaModel.deleteOne({_id:id})
-}
+app.put("/deleteById/:id", async (req, res) => {
+	let peliculaId = req.id;
+	await PeliculaModel.deleteOne({_id: peliculaId})
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
 
 //add();
 //update("6332067b02148ed6e38c9dd9");
 //removeById("6332093b8455b6fa6f112fdb")
-find();
+//find();
